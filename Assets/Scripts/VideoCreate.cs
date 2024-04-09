4cf812bf9535d35638f5d482622a3652a1e5f3a0
XRGeek <jameelqureshi2013@gmail.com>
2024-04-10 01:11:16 +0500

    5.2

--- Assets/Scripts/VideoCreate.cs
+++ Assets/Scripts/VideoCreate.cs
@@ -4,26 +4,92 @@ using UnityEngine;
 
 public class VideoCreate : MonoBehaviour
 {
+    public Material material;
+    public int index = 0;
+    public bool isImageDetected;
+
+    public GameObject[] texts;
+
+
     // Start is called before the first frame update
     void Start()
     {
         RotateImages();
+        isImageDetected = false;
+        foreach (GameObject text in texts)
+        {
+            text.SetActive(false);
+        }
     }
 
-    public Material material;
-    public int index = 0;
+    public void SetImageDetectionStatus(bool status)
+    {
+        isImageDetected = status;
+    }
+    
     // Update is called once per frame
 
     void RotateImages()
     {
-        
-        Texture tex = Resources.Load<Texture>("0-"+index);
-        material.mainTexture = tex;
-        index++;
-        if (index>677)
+        if (isImageDetected)
         {
-            return;
+            Texture tex = Resources.Load<Texture>("0-" + index);
+            material.mainTexture = tex;
+            index++;
+
+            if (index==52)
+            {
+                TurnOnText(0);
+            }
+            if (index == 90)
+            {
+                TurnOnText(1);
+            }
+            if (index == 130)
+            {
+                TurnOnText(2);
+            }
+            if (index == 170)
+            {
+                TurnOnText(3);
+            }
+
+            if (index == 210)
+            {
+                TurnOnText(4);
+            }
+
+            if (index == 240)
+            {
+                TurnOnText(5);
+            }
+            if (index == 255)
+            {
+                TurnOnText(6);
+            }
+            if (index == 260)
+            {
+                TurnOnText(7);
+            }
+
+           
+
+            if (index > 331)
+            {
+                return;
+            }
         }
+
+      
         Invoke(nameof(RotateImages), 0.05f);
+    }
+
+    void TurnOnText(int index)
+    {
+        foreach (GameObject text in texts)
+        {
+            text.SetActive(false);
+        }
+        texts[index].SetActive(true);
     }
 }
